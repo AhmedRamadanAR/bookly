2fe2926bfaee193ad85db278e1475d37e9669466
Ahmed Ramadan <ahmedramadan8050@gmail.com>
2025-01-03 00:37:08 +0200

    book entity adapter for hive

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,10 +1,30 @@
+import 'package:hive/hive.dart';
+
+
+part 'book_entity.g.dart';
+
+@HiveType(typeId: 1)
 class BookEntity {
+
+  @HiveField(0)
   final String bookId;
+
+  @HiveField(1)
   final String image;
+
+  @HiveField(2)
   final String title;
+
+  @HiveField(3)
   final String description;
+
+  @HiveField(4)
   final String authorName;
+
+  @HiveField(5)
   final num? price;
+
+  @HiveField(6)
   final num? rate;
 
   BookEntity( {required this.bookId,required this.image, required this.title, required this.description, required this.authorName,
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:bookly/core/utilis/api_service.dart';
 import 'package:bookly/core/utilis/app_router.dart';
-import 'package:bookly/features/splash/presentation/views/splash_screen.dart';
+import 'package:bookly/features/home/domain/entities/book_entity.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
-import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:hive_flutter/adapters.dart';
 
 import 'constants.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   ApiService.initDio();
-
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookEntityAdapter()); //
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
